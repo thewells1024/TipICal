e3e356437f59e27faa1d11b75b793565108470c5
Kent Kawahara <kkawahara1028@gmail.com>
2017-05-09 20:53:43 -0700

    Initial Commit

---
+++ TipICal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipICal
+//
+//  Created by Kent Kawahara on 5/9/17.
+//  Copyright © 2017 Kent Kawahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
